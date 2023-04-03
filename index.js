3bea9e5a8eda17fa7324db0a262935c3ce5743f9
prince1096 <princeraj10121996@gmail.com>
2023-04-03 17:37:42 +0530

    project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const ballRun = document.querySelector(".current-run");
 const playerRun = document.querySelector(".player-run");
 const playButton = document.querySelector(".play-button");
+const teamRun = document.querySelector(".team-run");
 
 let numRun;
 // console.log(numRun);
 
 let sum = 0;
+let totalRun = 0;
+let countOfWicket = 0;
 
 // ballRun.textContent = numRun;
 // playerRun.innerText = sum;
@@ -16,14 +19,46 @@ let sum = 0;
 // }
 
 const playerFunction = function () {
-  numRun = Math.trunc(Math.random() * 6) + 1;
-  sum = sum + numRun;
-  ballRun.innerText = numRun;
-  numRun === 5
-    ? (playerRun.innerText = `OUT at ${sum}`)
-    : (playerRun.innerText = sum);
-  //   playerRun.innerText = sum;
-  return sum;
+  if (countOfWicket < 10) {
+    numRun = Math.trunc(Math.random() * 6) + 1;
+
+    if (numRun === 5) {
+      countOfWicket = countOfWicket + 1;
+      console.log(countOfWicket);
+    }
+
+    if (numRun === 5) {
+      sum = sum;
+    } else {
+      sum = sum + numRun;
+    }
+
+    if (numRun === 5) {
+      totalRun = totalRun;
+    } else {
+      totalRun = totalRun + numRun;
+    }
+
+    // sum = sum + numRun;
+    ballRun.innerText = numRun;
+
+    if (numRun === 5) {
+      playerRun.innerHTML = `OUT at ${sum}`;
+      sum = 0;
+      return;
+    } else {
+      playerRun.innerHTML = sum;
+      teamRun.innerHTML = totalRun;
+    }
+
+    // numRun === 5
+    //   ? (playerRun.innerText = `OUT at ${sum}`)
+    //   : (playerRun.innerText = sum);
+    //   playerRun.innerText = sum;
+    return sum;
+  } else {
+    teamRun.innerHTML = ` All OUT at ${totalRun}`;
+  }
 };
 // console.log(playerFunction());
 
